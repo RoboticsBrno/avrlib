fdcda8989d7cef7bf12b21ad718183c570681e5d
Martin Vejnar <avakar@ratatanek.cz>
2009-07-30 12:39:10 +0200

    Stopwatch (and timeout) can now be suspended and resumed.

--- stopwatch.hpp
+++ stopwatch.hpp
@@ -10,23 +10,60 @@ public:
 	typedef typename Timer::time_type time_type;
 
 	explicit stopwatch(Timer const & timer)
-		: m_timer(timer)
+		: m_timer(timer), m_running(true)
 	{
 		this->clear();
 	}
 
 	void clear()
 	{
+		if (m_running)
+			m_base = m_timer();
+		else
+			m_base = 0;
+	}
+
+	void cancel()
+	{
+		m_running = false;
+		m_base = 0;
+	}
+
+	void restart()
+	{
+		m_running = true;
 		m_base = m_timer();
+	}
+
+	void start()
+	{
+		if (!m_running)
+		{
+			m_base = m_timer() - m_base;
+			m_running = true;
+		}
+	}
+
+	void stop()
+	{
+		if (m_running)
+		{
+			m_base = m_timer() - m_base;
+			m_running = false;
+		}
 	}
 
 	time_type operator()() const
 	{
-		return m_timer() - m_base;
+		if (m_running)
+			return m_timer() - m_base;
+		else
+			return m_base;
 	}
 
 private:
 	Timer const & m_timer;
+	bool m_running;
 	time_type m_base;
 };
 
