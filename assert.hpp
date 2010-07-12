87fefc2cea134e31fc8fd0baccf777ce0420a3af
Martin Vejnár <339713+avakar@users.noreply.github.com>
2010-07-12 10:14:26 +0200

    Some changes in assert.

--- assert.hpp
+++ assert.hpp
@@ -41,12 +41,14 @@ inline void trace_triggered(T value, char const * message, char const * file, in
 	trace_triggered((uint8_t const *)&value, sizeof(T), message, file, line);
 }
 
+namespace detail {
+
 template <typename Usart>
-void trace_main(Usart & usart, uint8_t const * value, uint8_t len, char const * message, char const * file, int line)
+void trace_assert_common_main(char const * header, Usart & usart, uint8_t const * value, uint8_t len, char const * message, char const * file, int line)
 {
 	cli();
 
-	send(usart, "trace: ");
+	send(usart, header);
 	send(usart, file);
 	send(usart, "(");
 	send_int(usart, line);
@@ -71,7 +73,7 @@ void trace_main(Usart & usart, uint8_t const * value, uint8_t len, char const * 
 		switch (usart.read())
 		{
 		case '?':
-			send(usart, "trace: ");
+			send(usart, header);
 			send(usart, file);
 			send(usart, "(");
 			send_int(usart, line);
@@ -85,8 +87,11 @@ void trace_main(Usart & usart, uint8_t const * value, uint8_t len, char const * 
 			}
 			send(usart, "\r\n");
 			break;
-		case 'r':
+		case 'q':
 			force_wd_reset();
+			break;
+		case 't':
+			trace_mask.set();
 			break;
 		case 'C':
 			trace_mask.clear();
@@ -98,45 +103,29 @@ void trace_main(Usart & usart, uint8_t const * value, uint8_t len, char const * 
 	}
 }
 
+}
+
+template <typename Usart>
+void trace_main(Usart & usart, uint8_t const * value, uint8_t len, char const * message, char const * file, int line)
+{
+	detail::trace_assert_common_main("trace: ", usart, value, len, message, file, line);
+}
+
 template <typename Usart>
 void assert_main(Usart & usart, char const * message, char const * file, int line)
 {
-	cli();
-	for (;;)
-	{
-		usart.process_rx();
-		usart.process_tx();
-
-		if (usart.empty())
-			continue;
-
-		switch (usart.read())
-		{
-		case '?':
-			send(usart, "assertion failed: ");
-			send(usart, file);
-			send(usart, "(");
-			send_int(usart, line);
-			send(usart, "): ");
-			send(usart, message);
-			send(usart, "\r\n");
-			break;
-		case 'r':
-			force_wd_reset();
-			break;
-		case 'c':
-			sei();
-			return;
-		}
-	}
+	trace_mask.set();
+	detail::trace_assert_common_main("assertion failed: ", usart, 0, 0, message, file, line);
 }
 
 }
 
 #ifndef NDEBUG
 #define AVRLIB_ASSERT(x) do { if(!(x)) ::avrlib::assertion_failed(#x, __FILE__, __LINE__); } while(0)
+#define AVRLIB_VERIFY(x) AVRLIB_ASSERT(x)
 #else
 #define AVRLIB_ASSERT(x) (void)0
+#define AVRLIB_VERIFY(x) (void)(x)
 #endif
 
 #ifndef NDEBUG
@@ -145,6 +134,14 @@ void assert_main(Usart & usart, char const * message, char const * file, int lin
 #else
 #define AVRLIB_TRACE(mask, value) (void)0
 #define AVRLIB_TRACE_BIN(mask, value, len) (void)0
+#endif
+
+#ifndef NDEBUG
+#define AVRLIB_ASSERT_TRACE(x, value) do { if(!(x)) { ::avrlib::trace_triggered((value), #value, __FILE__, __LINE__); ::avrlib::assertion_failed(#x, __FILE__, __LINE__); } } while(0)
+#define AVRLIB_VERIFY_TRACE(x, value) AVRLIB_ASSERT_TRACE(x, value)
+#else
+#define AVRLIB_ASSERT_TRACE(x, value) (void)0
+#define AVRLIB_VERIFY_TRACE(x, value) (void)(x)
 #endif
 
 #endif
