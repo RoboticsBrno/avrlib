55d67bf049398272a0b0fcdb2dfeb7b16a4e9d08
Martin Vejnár <339713+avakar@users.noreply.github.com>
2011-07-24 12:55:06 +0200

    Added avrlib::format.

--- format.hpp
+++ format.hpp
@@ -152,6 +152,139 @@ bool bufcmp(uint8_t const * buf, uint8_t len, char const (&pattern)[N])
 	return true;
 }
 
+template <typename Stream, typename Pattern>
+class format_impl
+{
+public:
+	format_impl(Stream & out, Pattern const & pattern)
+		: m_out(out), m_pattern(pattern)
+	{
+		this->write_literal();
+	}
+
+	format_impl & operator%(char const * str)
+	{
+		while (*str)
+			m_out.write(*str++);
+		m_pattern.pop();
+		this->write_literal();
+		return *this;
+	}
+
+	template <typename T>
+	format_impl & operator%(T const & t)
+	{
+		bool hex = m_pattern.top() == 'x';
+		if (hex)
+			send_hex(m_out, t);
+		else
+			send_int(m_out, t);
+		m_pattern.pop();
+		this->write_literal();
+		return *this;
+	}
+
+private:
+	void write_literal()
+	{
+		bool escape = false;
+		for (; !m_pattern.empty(); m_pattern.pop())
+		{
+			char ch = m_pattern.top();
+
+			if (ch == '%')
+			{
+				if (escape)
+				{
+					m_out.write('%');
+					m_out.write('%');
+					escape = false;
+				}
+				else
+				{
+					escape = true;
+				}
+			}
+			else
+			{
+				if (escape)
+					break;
+				m_out.write(ch);
+			}
+		}
+	}
+
+	Stream & m_out;
+	Pattern m_pattern;
+};
+
+class string_literal_range
+{
+public:
+	string_literal_range(char const * pattern)
+		: m_pattern(pattern)
+	{
+	}
+
+	bool empty() const
+	{
+		return *m_pattern == 0;
+	}
+
+	char top() const
+	{
+		return *m_pattern;
+	}
+
+	void pop()
+	{
+		++m_pattern;
+	}
+
+private:
+	char const * m_pattern;
+};
+
+class pgm_literal_range
+{
+public:
+	pgm_literal_range(prog_char const * first, prog_char const * last)
+		: m_first(first), m_last(last)
+	{
+	}
+
+	bool empty() const
+	{
+		return m_first == m_last;
+	}
+
+	char top() const
+	{
+		return pgm_read_byte(m_first);
+	}
+
+	void pop()
+	{
+		++m_first;
+	}
+
+private:
+	prog_char const * m_first;
+	prog_char const * m_last;
+};
+
+template <typename Stream>
+format_impl<Stream, string_literal_range> format(Stream & out, char const * pattern)
+{
+	return format_impl<Stream, string_literal_range>(out, string_literal_range(pattern));
+}
+
+template <typename Stream, int N>
+format_impl<Stream, pgm_literal_range> format_pgm(Stream & out, prog_char const (&pattern)[N])
+{
+	return format_impl<Stream, pgm_literal_range>(out, pgm_literal_range(pattern, pattern + N - 1));
+}
+
 }
 
 #endif
