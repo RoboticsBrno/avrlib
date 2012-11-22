d48395f47c65e10fb4ab91f03d57a0a7e27cf51f
Kuba Streit <streit@robotikabrno.cz>
2012-11-22 10:41:53 +0100

    add possibility to get timer1/3::ocr mode

---
+++ usart0.hpp
@@ -0,0 +1,105 @@
+#ifndef AVRLIB_USART0_HPP
+#define AVRLIB_USART0_HPP
+
+#include <avr/io.h>
+#include <stdint.h>
+
+#include "usart_base.hpp"
+#include "intr_prio.hpp"
+
+namespace avrlib {
+
+class usart0
+{
+public:
+	typedef uint8_t value_type;
+
+	usart0()
+	{
+	}
+
+	usart0(uint16_t ubrr, bool rx_interrupt = false)
+	{
+		this->open(ubrr, rx_interrupt);
+	}
+	
+	usart0(uint32_t speed, bool rx_interrupt = false)
+	{
+		this->open(speed, rx_interrupt);
+	}
+
+	~usart0()
+	{
+		this->close();
+	}
+
+	void open(uint32_t speed, bool rx_interrupt = false)
+	{
+		uint16_t ubrr = detail::get_ubrr(speed);
+		this->open(ubrr, rx_interrupt);
+	}
+	
+	void open(uint16_t ubrr, bool rx_interrupt = false)
+	{
+		UBRR0H = ubrr >> 8;
+		UBRR0L = ubrr & 0xFF;
+		UCSR0A = (1<<U2X0);
+		UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+
+		if (rx_interrupt)
+			UCSR0B |= (1<<RXCIE0);
+	}
+	
+	void rx_intr(intr_prio_t prio)
+	{
+		if (prio)
+			UCSR0B |= (1<<RXCIE0);
+		else
+			UCSR0B &= ~(1<<RXCIE0);
+	}
+
+#ifdef UMSEL00
+	void open_sync_slave(bool rx_interrupt)
+	{
+		UCSR0C = (1<<UMSEL00)|(1<<UCSZ01)|(1<<UCSZ00);
+		UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+
+		if (rx_interrupt)
+			UCSR0B |= (1<<RXCIE0);
+	}
+#endif
+
+	void close()
+	{
+		UCSR0B = 0;
+	}
+
+	void send(value_type v)
+	{
+		UDR0 = v;
+	}
+
+	bool overflow() const
+	{
+		return (UCSR0A & (1<<DOR0)) != 0;
+	}
+
+	value_type recv()
+	{
+		return UDR0;
+	}
+
+	bool rx_empty() const
+	{
+		return (UCSR0A & (1<<RXC0)) == 0;
+	}
+
+	bool tx_empty() const
+	{
+		return (UCSR0A & (1<<UDRE0)) != 0;
+	}
+};
+
+}
+
+#endif
---
+++ usart1.hpp
@@ -0,0 +1,105 @@
+#ifndef AVRLIB_USART1_HPP
+#define AVRLIB_USART1_HPP
+
+#include <avr/io.h>
+#include <stdint.h>
+
+#include "usart_base.hpp"
+#include "intr_prio.hpp"
+
+namespace avrlib {
+
+class usart1
+{
+public:
+	typedef uint8_t value_type;
+	
+	usart1()
+	{
+	}
+
+	usart1(uint16_t ubrr, bool rx_interrupt = false)
+	{
+		this->open(ubrr, rx_interrupt);
+	}
+	
+	usart1(uint32_t speed, bool rx_interrupt = false)
+	{
+		this->open(speed, rx_interrupt);
+	}
+
+	~usart1()
+	{
+		this->close();
+	}
+
+	void open(uint32_t speed, bool rx_interrupt = false)
+	{
+		uint16_t ubrr = detail::get_ubrr(speed);
+		this->open(ubrr, rx_interrupt);
+	}
+
+	void open(uint16_t ubrr, bool rx_interrupt = false)
+	{
+		UBRR1H = ubrr >> 8;
+		UBRR1L = ubrr & 0xFF;
+		UCSR1A = (1<<U2X1);
+		UCSR1B = (1<<RXEN1)|(1<<TXEN1);
+
+		if (rx_interrupt)
+			UCSR1B |= (1<<RXCIE1);
+	}
+
+	void rx_intr(intr_prio_t prio)
+	{
+		if (prio)
+			UCSR1B |= (1<<RXCIE1);
+		else
+			UCSR1B &= ~(1<<RXCIE1);
+	}
+
+#ifdef UMSEL10
+	void open_sync_slave(bool rx_interrupt)
+	{
+		UCSR1C = (1<<UMSEL10)|(1<<UCSZ11)|(1<<UCSZ10);
+		UCSR1B = (1<<RXEN1)|(1<<TXEN1);
+
+		if (rx_interrupt)
+			UCSR1B |= (1<<RXCIE1);
+	}
+#endif
+
+	void close()
+	{
+		UCSR1B = 0;
+	}
+
+	void send(value_type v)
+	{
+		UDR1 = v;
+	}
+
+	bool overflow() const
+	{
+		return (UCSR1A & (1<<DOR1)) != 0;
+	}
+
+	value_type recv()
+	{
+		return UDR1;
+	}
+
+	bool rx_empty() const
+	{
+		return (UCSR1A & (1<<RXC1)) == 0;
+	}
+
+	bool tx_empty() const
+	{
+		return (UCSR1A & (1<<UDRE1)) != 0;
+	}
+};
+
+}
+
+#endif
