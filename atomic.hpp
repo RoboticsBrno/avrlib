28837b003842675e95b07ffb629ea06de87be23d
Martin Vejn?r <avakar@ratatanek.cz>
2013-02-06 21:11:52 +0100

    Added support for the xmega's new LAC, LAS and LAT instructions.

--- atomic.hpp
+++ atomic.hpp
@@ -1,6 +1,18 @@
 #ifndef AVRLIB_ATOMIC_HPP
 #define AVRLIB_ATOMIC_HPP
 
+// Atomic `*ptr &= ~mask`
+#define avrlib_atomic_clear(ptr, mask) \
+    asm volatile ("lac Z, %1":: "z"(ptr), "r"((uint8_t)(mask)): "memory")
+
+// Atomic `*ptr |= mask`
+#define avrlib_atomic_set(ptr, mask) \
+    asm volatile ("las Z, %1":: "z"(ptr), "r"((uint8_t)(mask)): "memory")
+
+// Atomic `*ptr ^= mask`
+#define avrlib_atomic_toggle(ptr, mask) \
+    asm volatile ("lat Z, %1":: "z"(ptr), "r"((uint8_t)(mask)): "memory")
+
 namespace avrlib {
 
 template <typename T>
