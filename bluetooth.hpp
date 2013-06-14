b1415d5392352a74887a481f2a68454057f379c3
Jaroslav Páral <6726556+JarekParal@users.noreply.github.com>
2013-06-14 22:45:28 +0200

    Added method disconnect to bluetooth.hpp and edited stopwatch.hpp - by Kubas

--- bluetooth.hpp
+++ bluetooth.hpp
@@ -1,9 +1,11 @@
 #ifndef AVRLIB_BLUETOOTH_HPP
 #define AVRLIB_BLUETOOTH_HPP
 
+#include "stopwatch.hpp"
+
 namespace avrlib {
 	
-volatile uint8_t addr[][11] =
+volatile uint8_t addr[][13] =
 {
 {0x00, 0x12, 0xf3, 0x07, 0xbf, 0xf3},//0 - newBT0
 {0x00, 0x12, 0xf3, 0x07, 0xc0, 0x32},//1 - Yunitooth_new_1
@@ -15,8 +17,9 @@ volatile uint8_t addr[][11] =
 {0x00, 0x12, 0xf3, 0x04, 0xa9, 0xfa},//7 - Spaintooth
 {0x00, 0x12, 0xf3, 0x07, 0xbf, 0xf4},//8 - Yunitooth_new_2
 {0x00, 0x12, 0xf3, 0x04, 0xcd, 0x80},//9 - yunitooth_long_1
-{0x00, 0x12, 0xf3, 0x04, 0xaa, 0x06} //10 - Avakar's Yunimin
-
+{0x00, 0x12, 0xf3, 0x04, 0xaa, 0x06},//10 - Avakar's Yunimin
+{0x00, 0x12, 0xf3, 0x0e, 0xb3, 0x76},//11 - Gimply = Enkeli (Vojta Urban)
+{0x00, 0x12, 0xf3, 0x18, 0x2a, 0x3b} //12 - Yunibeer = Alwood (Franta Èaslavský)	
 };	
 	
 template <typename Usart>
@@ -26,8 +29,8 @@ class bt_t
 	typedef Usart usart_type;
 	
 	bt_t(usart_type &s)
-	:m_usart(s)
-{}
+		:m_usart(s), m_connected(false)
+	{}
 	
 	bool connect(uint8_t a)
 	{
@@ -62,6 +65,23 @@ class bt_t
 		send(m_usart, "AT*ADDM\r");	//Data Mode
 		m_usart.flush();
 		
+		m_connected = true;
+		return true;
+	}
+	
+	bool disconnect()
+	{
+		if(!m_connected)
+			return true;
+		_delay_ms(1100);
+		send(m_usart, "///");
+		m_usart.flush();
+		_delay_ms(1100);
+		send(m_usart, "AT*ADNRP=0,0\r");
+		m_usart.flush();
+		send(m_usart, "AT*ADDM\r");
+		m_usart.flush();
+		m_connected = false;
 		return true;
 	}
 	
@@ -99,8 +119,11 @@ class bt_t
 		return true;
 	}
 	
-	private:
+	bool connected() { return m_connected; }
+	
+private:
 	usart_type &m_usart;
+	bool m_connected;
 };
 	
 }
