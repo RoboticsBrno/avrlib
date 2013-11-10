26a29bddbea7ff2abbdff7f754ef2f496111126a
Kuba Streit <streit@robotikabrno.cz>
2013-11-10 17:37:57 +0100

    uart_xmega now supports 5-9 data bits, all parity options and 1 or 2 stop-bits selection

--- uart_xmega.hpp
+++ uart_xmega.hpp
@@ -5,6 +5,22 @@
 #include <stdint.h>
 
 namespace avrlib {
+	
+enum uart_data_bits_t
+{
+	uart_5_bits = USART_CHSIZE_5BIT_gc,
+	uart_6_bits = USART_CHSIZE_6BIT_gc,
+	uart_7_bits = USART_CHSIZE_7BIT_gc,
+	uart_8_bits = USART_CHSIZE_8BIT_gc,
+	uart_9_bits = USART_CHSIZE_9BIT_gc
+};
+
+enum uart_parity_t
+{
+	uart_no_parity = USART_PMODE_DISABLED_gc,
+	uart_even_parity = USART_PMODE_EVEN_gc,
+	uart_odd_parity = USART_PMODE_ODD_gc
+};
 
 class uart_xmega
 {
@@ -16,7 +32,8 @@ public:
 	{
 	}
 
-	void open(USART_t & p, uint16_t baudrate, bool rx_interrupt = false, bool synchronous = false)
+	void open(USART_t & p, uint16_t baudrate, bool rx_interrupt = false, bool synchronous = false,
+		uart_data_bits_t data_bits = uart_8_bits, bool stopbit2 = false, uart_parity_t parity = uart_no_parity)
 	{
 		m_p = &p;
 		this->set_speed(baudrate);
@@ -25,7 +42,8 @@ public:
 		else
 			m_p->CTRLA = 0;
 		m_p->CTRLC = (synchronous? USART_CMODE_SYNCHRONOUS_gc: USART_CMODE_ASYNCHRONOUS_gc)
-			| (3<<USART_CHSIZE_gp);
+			| (data_bits & USART_CHSIZE_gm) | (parity & USART_PMODE_gm)
+			| (stopbit2<<USART_SBMODE_bp);
 		m_p->CTRLB = USART_RXEN_bm | USART_TXEN_bm;
 	}
 
@@ -35,6 +53,8 @@ public:
 		m_p->CTRLB = 0;
 		m_p->CTRLC = USART_CMODE_ASYNCHRONOUS_gc | (3<<USART_CHSIZE_gp);
 	}
+	
+	bool is_open() const { return m_p->CTRLB != 0; }
 
 	void set_speed(uint16_t baudrate)
 	{
@@ -83,6 +103,24 @@ public:
 			return false;
 		m_p->STATUS = USART_TXCIF_bm;
 		return true;
+	}
+	
+	bool bit9() const
+	{
+		return (m_p->STATUS & USART_RXB8_bm) != 0;
+	}
+	
+	void bit9(const bool& value)
+	{
+		if(value)
+			m_p->CTRLB |= USART_TXB8_bm;
+		else
+			m_p->CTRLB &= ~USART_TXB8_bm;
+	}
+	
+	uart_data_bits_t data_bits() const
+	{
+		return uart_data_bits_t(m_p->CTRLC & USART_CHSIZE_gm);
 	}
 	
 private:
