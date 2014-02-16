385a7e6e7debedf78cb8959a2f63ed6dcefdceac
Martin Vejn?r <avakar@ratatanek.cz>
2014-02-16 13:03:52 +0100

    Something DFU related.

--- dfusuffix.py
+++ dfusuffix.py
@@ -7,19 +7,31 @@ class YbPrefix:
     def __len__(self):
         return sum((4 + len(val) for key, val in self.data))
 
+    def clear(self):
+        del self.data[:]
+
     def load(self, stream):
         data = []
         while True:
-            size, key = struct.unpack('<HH', stream[:4])
-            value = stream[4:size]
+            size, key = struct.unpack('<BB', stream[:2])
+            value = stream[2:size+1]
             data.append((key, value))
-            stream = stream[size:]
+            stream = stream[size+1:]
             if key == 0:
                 break
         self.data = data
 
-    def store(self):
-        return ''.join((struct.pack('<HH', 4+len(value), key) + value for key, value in self.data))
+    def store(self, padding=None):
+        unpadded = ''.join((struct.pack('<BB', 1+len(value), key) + value for key, value in self.data)) + '\x00'
+
+        total_size = len(unpadded)
+        pad = 0
+        if padding:
+            inverse_gap = total_size % padding
+            if inverse_gap:
+                pad = padding - inverse_gap
+
+        return unpadded + '\x00'*pad
 
     def add(self, key, value):
         self.data.append((key, value))
@@ -78,7 +90,7 @@ class Suffix:
         crc = self._calc_crc(part, crc)
         return s + part + struct.pack('<I', crc)
 
-def add_suffix(fin, fout, idVendor, idProduct, bcdDevice, yb_file={}, force=False):
+def add_suffix(fin, fout, idVendor, idProduct, bcdDevice, force=False):
     data = fin.read()
 
     if not force:
@@ -87,16 +99,8 @@ def add_suffix(fin, fout, idVendor, idProduct, bcdDevice, yb_file={}, force=Fals
             print >>sys.stderr, 'There already seems to be a suffix in the file (--force to override).'
             return 1
 
-    yb_prefix = YbPrefix()
-    if yb_file is not None:
-        device_guid = uuid.UUID(yb_file['device_guid']).bytes
-        yb_prefix.add(1, device_guid)
-        if 'fw_timestamp' in yb_file:
-            yb_prefix.add(2, struct.pack('<I', yb_file['fw_timestamp']))
-        yb_prefix.finish(yb_file.get('dfu_padding'))
-
     suf = Suffix(idVendor, idProduct, bcdDevice)
-    data = suf.store(yb_prefix.store() + data)
+    data = suf.store(data)
 
     fout.write(data)
     return 0
@@ -125,7 +129,7 @@ def verify_suffix(input, idVendor, idProduct, bcdDevice):
         yb_prefix.load(data)
         for key, value in yb_prefix.data:
             if key == 1:
-                print 'yb:device_guid = %s' % uuid.UUID(bytes=value)
+                print 'yb:device_guid = %s' % ''.join(['%02x' % v for v in value])
             elif key == 2:
                 print 'yb:fw_timestamp = %s' % datetime.fromtimestamp(struct.unpack('<I', value)[0]).ctime()
             elif key != 0:
@@ -133,7 +137,7 @@ def verify_suffix(input, idVendor, idProduct, bcdDevice):
 
     return 0
 
-def remove_suffix(input, output, idVendor, idProduct, bcdDevice, yb, force):
+def remove_suffix(input, output, idVendor, idProduct, bcdDevice, force):
     with open(input, 'rb') as fin:
         data = fin.read()
 
@@ -148,19 +152,7 @@ def remove_suffix(input, output, idVendor, idProduct, bcdDevice, yb, force):
         print 'There seems to be a DFU suffix, but the CRC is invalid.'
         return 2
 
-    yb_prefix = YbPrefix()
-    if (suf.idVendor, suf.idProduct) == (0x4a61, 0x679c):
-        if not yb and not force:
-            print 'The suffix indicates an yb device, but --yb is not specified (--force to override).'
-            return 1
-    if yb and not force and (suf.idVendor not in (0x4a61, 0xffff) or suf.idProduct not in (0xffff, 0x679c)):
-        print 'The suffix indicates a non-yb device, but --yb is specified (--force to override).'
-        return 1
-
-    if yb:
-        yb_prefix.load(data)
-
-    data = data[len(yb_prefix):-len(suf)]
+    data = data[0:-len(suf)]
     with open(output, 'wb') as fout:
         fout.write(data)
     return 0
@@ -172,9 +164,7 @@ def _main():
     ap.add_argument('output', nargs='?')
     ap.add_argument('--vidpid', '-v', default='FFFF:FFFF:FFFF')
     ap.add_argument('--add', '-A', action='store_true', default=False)
-    ap.add_argument('--yb-file', '-Y', type=argparse.FileType('r'))
     ap.add_argument('--remove', '-R', action='store_true', default=False)
-    ap.add_argument('--yb', '-y', action='store_true', default=False)
     ap.add_argument('--force', '-f', action='store_true', default=False)
     args = ap.parse_args()
 
@@ -193,33 +183,17 @@ def _main():
         print >>sys.stderr, '--vidpid must be either vid:pid or vid:pid:rev'
         return 1
 
-    yb_file = None
-    if args.yb_file:
-        if not args.add:
-            print >>sys.stderr, '--yb-file can only be used with --add'
-            return 1
-        if not args.force:
-            if idVendor not in (0xffff, 0x4a61) or idProduct not in (0xffff, 0x679c):
-                print >>sys.stderr, 'vid:pid does not indicate an yb device (--force to override).'
-                return 1
-            idVendor, idProduct = (0x4a61, 0x679c)
-        yb_file = json.load(args.yb_file)
-
-    if not args.force and args.add and (idVendor, idProduct) == (0x4a61, 0x679c) and not args.yb_file:
-        print >>sys.stderr, 'vid:pid indicates an yb device, but --yb-file was not given (--force to override).'
-        return 1
-
     if not args.output:
         args.output = args.input
 
     if args.add:
         ff = StringIO.StringIO()
         with open(args.input, 'rb') as fin:
-            res = add_suffix(fin, ff, idVendor, idProduct, bcdDevice, yb_file, args.force)
+            res = add_suffix(fin, ff, idVendor, idProduct, bcdDevice, args.force)
         with open(args.output, 'wb') as fout:
             fout.write(ff)
     elif args.remove:
-        return remove_suffix(args.input, args.output, idVendor, idProduct, bcdDevice, args.yb, args.force)
+        return remove_suffix(args.input, args.output, idVendor, idProduct, bcdDevice, args.force)
     else:
         return verify_suffix(args.input, idVendor, idProduct, bcdDevice)
 
